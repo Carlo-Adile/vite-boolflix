87d2ab032a7b81a7a8218edf5a4272adac7f17b3
Carlo-Adile <adile.carlo.a@gmailcom>
2024-04-03 12:26:15 +0200

    adding base structure of components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,47 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script>
-export default {
-  setup () {
-    
+import { ref } from 'vue';
 
-    return {}
+export default {
+  name: "Header",
+  data() {
+    return {
+      searchQuery: ''
+    }
+  },
+  methods: {
+    search(){
+      /* chiamata all'API */
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <div id="site_header">
+    <h4>Header: Navbar</h4>
+    <nav class="navbar expand-lg p-4">
+      <a href="#" class="navbar-brand">
+        <img src="../assets/vue.svg" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
+        Navbar Logo</a>
+      <form role="search" class="d-flex">
+        <input v-model="searchQuery" class="form-control me-2" type="text" placeholder="Cerca un film tramite la nostra API!" aria-label="Search">
+        <button @click="search" class="btn btn-outline-success" type="submit">Search</button>
+      </form>
 
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#site_header {
+  width: 100%;
+  height: 200px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  padding: 1rem;
+  border: 1px dashed black;
+}
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "Main",
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="site_main">
+    <h4>Main: Cards</h4>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#site_main{
+  width: 100%;
+  padding: 1rem;
+  border: 1px dashed black;
+
+}
+</style>
